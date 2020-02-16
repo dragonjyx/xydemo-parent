4b3346525b22883b7ba276f33cef697b2e0d4b23
dragonyeah <674806334@qq.com>
2020-02-17 00:06:49 +0800

    add util and common to spring boot 讲课demo

--- xydemo-web-backend/src/main/java/com/web/config/WebConfig.java
+++ xydemo-web-backend/src/main/java/com/web/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.web.config;
 
 import com.xydemo.utils.config.WebMvcConfig;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -11,8 +10,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  * 其他配置
  */
 @MapperScan(basePackages = "com.web.mapper")
-@ComponentScans(value = {@ComponentScan("com.web.service.impl"),
-        @ComponentScan("com.web.dao")})
+@ComponentScan("com.web.dao")
 @Configuration
 public class WebConfig extends WebMvcConfig {
 
